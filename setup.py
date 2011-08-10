17b8454a75a13814a1cfaa61eb5e7708e0a3d722
Teren Teh <tereno@gmail.com>
2011-08-09 22:03:18 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name = 'financialpy',
+    packages = ['financialpy'],
+    version = '0.0.1',
+    description = 'Functions and objects related to the financial industry',
+    author = 'Teren Teh',
+    author_email = 'teren@tixocloud.com',
+    url = 'http://tixocloud.com',
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ]
+)
